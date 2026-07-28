e66e7d1fbdfea0d66721db1df5ca4d5ca7c16441
Sohail Issa <issasohail@gmail.com>
2026-07-28 21:18:09 +0500

    remove backup filese

--- .codex_file_backups/20260524-152644-context-processor-cache/context_processors.py
+++
@@ -1,9 +0,0 @@
-from .models import GlobalSettings
-from .currency import currency_symbol
-
-def global_settings(request):
-    settings_obj = GlobalSettings.get_solo()
-    return {
-        "GLOBAL_SETTINGS": settings_obj,
-        "CURRENCY_SYMBOL": currency_symbol(settings_obj),
-    }

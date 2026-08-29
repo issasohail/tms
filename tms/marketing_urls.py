1dcb60cdab986bab2f89ebeb35055a23417bf996
Sohail Issa <issasohail@gmail.com>
2026-08-29 13:33:02 +0500

    Expose public photo links on marketing domain

--- tms/marketing_urls.py
+++ tms/marketing_urls.py
@@ -1,6 +1,17 @@
 from django.urls import include, path
 
+from properties import views as property_views
 
 urlpatterns = [
+    path(
+        "p/<str:token>/",
+        property_views.public_photo_link,
+        name="public_photo_link",
+    ),
+    path(
+        "p/<str:token>/<int:media_id>/",
+        property_views.public_photo_file,
+        name="public_photo_link_file",
+    ),
     path("", include("marketing.urls")),
 ]
